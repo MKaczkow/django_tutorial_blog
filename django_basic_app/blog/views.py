e83262057e47b36d9d3301e98cdacaf62a82d909
MKaczkow <maciej.kaczkowski.stud@pw.edu.pl>
2021-07-22 12:29:15 +0200

    db orm basics

--- django_basic_app/blog/views.py
+++ django_basic_app/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {'author': 'lorca',
-     'title': 'dummy post',
-     'content': 'test bla bla bla lorem ipsum',
-     'date_proceded': 2021},
-
-    {'author': 'somebody',
-     'title': 'not important',
-     'content': 'lorem ipsum',
-     'date_proceded': 2020}
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
