5093a9cf229fa168f01ef4c8c858b2b4822782ce
MKaczkow <maciej.kaczkowski.stud@pw.edu.pl>
2021-07-22 11:44:49 +0200

    routing correction, css, js added from templates, admin page

--- django_basic_app/blog/views.py
+++ django_basic_app/blog/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {'author': 'lorca',
+     'title': 'dummy post',
+     'content': 'test bla bla bla lorem ipsum',
+     'date_proceded': 2021},
+
+    {'author': 'somebody',
+     'title': 'not important',
+     'content': 'lorem ipsum',
+     'date_proceded': 2020}
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
